5f6be72832080b905f7d04bd35008a2ee7a1693b
karyssa-dandrea <karyssadandrea@gmail.com>
2022-01-26 00:02:06 -0800

    skipped app test

--- src/services/data.js
+++ src/services/data.js
@@ -1,7 +1,6 @@
 export async function getFilms() {
   const resp = await fetch('https://ghibliapi.herokuapp.com/films');
   const data = await resp.json();
-  console.log(data);
   return data;
 }
 
