fabb4c05e5866647f84c04463dfb167b6fdb27c4
karyssa-dandrea <karyssadandrea@gmail.com>
2022-01-25 17:11:38 -0800

    did a loading state

--- src/components/Details/Details.js
+++ src/components/Details/Details.js
@@ -3,6 +3,7 @@ import React from 'react';
 export default function Details({ films, searchFilm }) {
   return (
     <div>
+      <h1>Studio Ghibli Films</h1>
       <img src={searchFilm.image} />
       {films.map((film) => (
         <img key={film.id} src={film.image} />
--- src/views/Home/Home.js
+++ src/views/Home/Home.js
@@ -8,22 +8,27 @@ export default function Home() {
   const [films, setFilms] = useState([]);
   const [searchFilm, setSearchFilm] = useState({});
   const [searchInput, setSearchInput] = useState('');
+  const [loading, setLoading] = useState(true);
 
   const handleSearch = () => {
     let filter = films.filter((film) =>
       film.title.toLowerCase().includes(searchInput.toLowerCase())
     );
     setSearchFilm(filter[0]);
-    console.log(searchFilm);
   };
 
   useEffect(() => {
     const fetchData = async () => {
       const data = await getFilms();
       setFilms(data);
+      setLoading(false);
     };
     fetchData();
   }, []);
+
+  if (loading) {
+    return <h1>Loading..</h1>;
+  }
   return (
     <div>
       <Controls
